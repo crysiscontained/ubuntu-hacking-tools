2040716b073c291792c06c11ea1ff58067c0fdfa
Crysis Contained <crysisconained.dev@gmail.com>
2022-06-12 21:23:35 -0400

    Changed from global install to user

--- ToolsInstall.sh
+++ ToolsInstall.sh
@@ -22,7 +22,7 @@ if [ -d $GOPATH ]; then
   echo "Go is installed"
   sleep 1
 
-elif [[ -z "$GOPATH" ]]; then
+else
   echo "go is not installed, would you like to install?"
   PS3="Please select an option (use numbers) "
   choices=("yes" "no")
@@ -60,82 +60,83 @@ echo "Done"
 
 #JSParser
 echo "Installing JSParser"
-cd /opt
-sudo git clone https://github.com/nahamsec/JSParser.git
-cd /opt/JSParser
+mkdir ~/tools
+cd ~/tools
+git clone https://github.com/nahamsec/JSParser.git
+cd ~/tools/JSParser
 sudo python3 setup.py install
 echo "Done"
 
 #Sublist3r
-cd /opt
+cd ~/tools
 echo "Installing Sublist3r"
-sudo git clone https://github.com/aboul3la/Sublist3r.git
-cd /opt/Sublist3r
+git clone https://github.com/aboul3la/Sublist3r.git
+cd ~/tools/Sublist3r
 pip install -r requirements.txt
-cd /opt
+cd ~/tools
 echo "Done"
 
 #teh_s3_bucketeers
-cd /opt
+cd ~/tools
 echo "Installing teh_s3_bucketeers"
-sudo git clone https://github.com/tomdev/teh_s3_bucketeers.git
+git clone https://github.com/tomdev/teh_s3_bucketeers.git
 echo "Done"
 
 #wpscan
-cd /opt
+cd ~/tools
 echo "Installing wpscan"
-sudo git clone https://github.com/wpscanteam/wpscan.git
-cd /opt/wpscan
-sudo gem install bundler && sudo bundle install --local
-cd /opt
+git clone https://github.com/wpscanteam/wpscan.git
+cd ~/tools/wpscan
+gem install bundler && bundle install --local
+cd ~/tools
 echo "Done"
 
 #dirsearch
-cd /opt
+cd ~/tools
 echo "Installing dirsearch"
-sudo git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/maurosoria/dirsearch.git
 echo "Done"
 
 #lazys3
-cd /opt
+cd ~/tools
 echo "Installing lazys3"
-sudo git clone https://github.com/nahamsec/lazys3.git
+git clone https://github.com/nahamsec/lazys3.git
 echo "Done"
 
 #virtual host discovery
-cd /opt
+cd ~/tools
 echo "Installing virtual host discovery"
-sudo git clone https://github.com/jobertabma/virtual-host-discovery.git
+git clone https://github.com/jobertabma/virtual-host-discovery.git
 echo "Done"
 
 #sqlmap
-cd /opt
+cd ~/tools
 echo "Installing sqlmap"
-sudo git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
 echo "Done"
 
 #knock.py
-cd /opt
+cd ~/tools
 echo "Installing Knock.py"
-sudo git clone https://github.com/guelfoweb/knock.git
+git clone https://github.com/guelfoweb/knock.git
 echo "Done"
 
 #massdns
-cd /opt
+cd ~/tools
 echo "Installing massdns"
-sudo git clone https://github.com/blechschmidt/massdns.git
-cd /opt/massdns
-sudo make
-cd /opt
+git clone https://github.com/blechschmidt/massdns.git
+cd ~/tools/massdns
+make
+cd ~/tools
 echo "Done"
 
 #asnlookup
-cd /opt
+cd ~/tools
 echo "installing asnlookup"
-sudo git clone https://github.com/yassineaboukir/asnlookup.git
-cd /opt/asnlookup
+git clone https://github.com/yassineaboukir/asnlookup.git
+cd ~/tools/asnlookup
 pip install -r requirements.txt
-cd /opt
+cd ~/tools
 echo "Done"
 
 #httprobe
@@ -163,9 +164,9 @@ go install github.com/tomnomnom/waybackurls@latest
 echo "Done"
 
 #crtndstry
-cd /opt
+cd ~/tools
 echo "Installing crtndstry"
-sudo git clone https://github.com/nahamsec/crtndstry.git
+git clone https://github.com/nahamsec/crtndstry.git
 echo "Done"
 
 #seclists
@@ -176,19 +177,19 @@ git clone https://github.com/danielmiessler/SecLists.git
 cd ~/Documents/wordlists/SecLists/Discovery/DNS
 echo "Cleaning dns-jhaddix.txt so it doesn't break massdns"
 cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-cd /opt
+cd ~/tools
 echo "Done"
 
 #JohntheRipper
 echo "Installing John the Ripper"
 sudo apt -y install yasm pkg-config libgmp-dev libpcap-dev libbz2-dev
-cd /opt
-sudo mkdir /opt/johntheripper
-cd /opt/johntheripper
-sudo git clone https://github.com/openwall/john -b bleeding-jumbo john
-cd /opt/johntheripper/john/src
-sudo ./configure && sudo make -s clean && sudo make -sj4
-sudo ln -sf /opt/johntheripper/john/run/john /usr/bin/john
+cd ~/tools
+mkdir ~/tools/johntheripper
+cd ~/tools/johntheripper
+git clone https://github.com/openwall/john -b bleeding-jumbo john
+cd ~/tools/johntheripper/john/src
+./configure && make -s clean && make -sj4
+sudo ln -sf ~/tools/johntheripper/john/run/john /usr/bin/john
 echo "Done"
 
 #ffuf
@@ -203,30 +204,29 @@ echo "Done"
 
 #feroxbuster
 echo "Installing Feroxbuster"
-cd /opt
-sudo curl -sL https://raw.githubusercontent.com/epi052/feroxbuster/master/install-nix.sh | sudo bash
+cd ~/tools
+curl -sL https://raw.githubusercontent.com/epi052/feroxbuster/master/install-nix.sh | sudo bash
 echo "Done"
 
 #searchsploit
 echo "Installing searchsploit"
-sudo git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
-sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
+git clone https://github.com/offensive-security/exploitdb.git ~/tools/exploitdb
+sudo ln -sf ~/tools/exploitdb/searchsploit /usr/local/bin/searchsploit
 echo "Done"
 
 #impacket
 echo "Installing impacket"
 python3 -m pip install impacket
-cd /opt
-sudo git clone https://github.com/SecureAuthCorp/impacket
+cd ~/tools
+git clone https://github.com/SecureAuthCorp/impacket
 echo "Done"
 
 #metasploit
 echo "Installing the Metasploit Framework"
 cd ~/Downloads
-sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 sudo chmod 755 ~/Downloads/msfinstall
 sudo ~/Downloads/msfinstall
-sudo ln -sf /opt/metasploit-framework/bin/msfconsole /usr/bin/msfconsole
 rm ~/Downloads/msfinstall
 echo "Done"
 
@@ -234,10 +234,10 @@ echo "Done"
 echo "Downloading Hydra"
 cd ~/Downloads
 wget https://github.com/vanhauser-thc/thc-hydra/archive/refs/tags/v9.3.tar.gz
-sudo tar -C /opt -xzf ~/Downloads/v9.3.tar.gz
-cd /opt/thc-hydra-9.3
-sudo ./configure && sudo make && sudo make install
-cd /opt
+tar -C ~/tools -xzf ~/Downloads/v9.3.tar.gz
+cd ~/tools/thc-hydra-9.3
+./configure && make && sudo make install
+cd ~/tools
 echo "Done"
 
 #zaproxy
@@ -250,6 +250,6 @@ sudo ~/Downloads/ZAP_2_11_1_unix.sh
 rm ~/Downloads/ZAP_2_11_1_unix.sh
 echo "Done"
 
-echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in either ~/go/bin or in your /opt folder"
+echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in either ~/go/bin or in your ~/tools folder"
 echo "nmap, searchsploit, netcat, John the Ripper, Metasploit, and Hydra are already in bin and can be called anywere"
 echo "aws credintials can be setup in ~/.aws"
